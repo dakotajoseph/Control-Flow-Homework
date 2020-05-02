d3f83ae2662d6d18821da5f1f44128f8c65f51a8
dakotajoseph <numerologyseven@yahoo.com>
2020-05-02 19:56:25 -0400

    Upload control flow homework project

---
+++ ControlFlow/src/ControlFlow1.java
@@ -0,0 +1,16 @@
+public class ControlFlow1 {
+
+	public static void main(String[] args) {
+		
+		String [] asterisk = {"*", "**", "***", "****", "*****"};
+		
+		
+		for (String asterickNumber : asterisk) {
+			
+			System.out.println(asterickNumber);
+			} 
+		
+	
+		}
+		
+		}
